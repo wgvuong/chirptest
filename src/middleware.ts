6b45d5b862b56dda80577f5abb302dfec976148f
William Vuong <wvuong120@gmail.com>
2023-07-20 17:00:44 -0700

    Add auth

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export default authMiddleware( {
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
