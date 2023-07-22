5bfa87d7e9bea943f805289992bd89caff3ecb88
William Vuong <wvuong120@gmail.com>
2023-07-21 20:41:09 -0700

    Router

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,8 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export default authMiddleware( {
-  afterAuth(auth, req) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    return NextResponse.next();
-  }
+export default authMiddleware({
+  afterAuth() {}
 });
 
 export const config = {
