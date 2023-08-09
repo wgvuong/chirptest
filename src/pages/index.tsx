28107b43a2b03146468c29e4f944aec90ea22936
William Vuong <wvuong120@gmail.com>
2023-08-09 00:37:14 -0700

    Loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ const PostView = (props: PostWithUser) => {
             <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
           </span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-xl">{post.content}</span>
       </div>
     </div>
   );
